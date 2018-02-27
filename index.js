c1ca1e2e971fc4fc6245f8a4255800b1ae0f5384
Raiondesu <a.raiondesu@gmail.com>
2018-02-27 19:20:31 +0300

    Added tests and types fixes.

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-export * from './';
+import DaDataModel from './models/dadata'
 
-import DaDataModel from "./models/dadata"
+export default class DaData {
+  constructor (API_KEY) {
+    this.model = new DaDataModel(this, API_KEY);
+  }
 
+  suggest (type, query/* , count, options */) {
+    return this.model.suggest(type, { query, count: arguments[2], ...(arguments[3] || {}) });
+  }
+
+  detectAddressByIP () {
+    return this.model.detectAddressByIP(arguments[0]);
+  }
+}
---
+++ tests/model.spec.js
@@ -0,0 +1,5 @@
+describe('DaDataModel', function () {
+  test('does requests', function () {
+
+  })
+})
