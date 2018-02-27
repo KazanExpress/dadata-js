eeb43689364e2fa2112081c7abdb0c1e67df7530
Raiondesu <a.raiondesu@gmail.com>
2018-02-27 19:51:11 +0300

    TESTS AND FREAKIN BABEL

--- tests/model.spec.js
+++ tests/model.spec.js
@@ -1,5 +1,9 @@
-describe('DaDataModel', function () {
-  test('does requests', function () {
+import DaDataModel from '../models/dadata'
 
+describe('DaDataModel', () => {
+  it('sets token', () => {
+    const model = new DaDataModel(null, 'asdasdasd')
+
+    expect(model.token).toBe('asdasdasd')
   })
 })
