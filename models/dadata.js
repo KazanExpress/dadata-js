8fd6a0f7cedb4e76ed5503bcfd7390a9d841d12c
Karen <dev.kaskar2008@gmail.com>
2018-02-27 19:25:21 +0300

    extended DaDataModel functionality

--- models/dadata.js
+++ models/dadata.js
@@ -1,13 +1,35 @@
 import BaseModel from 'basemodelts'
 
+const DADATA_API_URL = 'https://suggestions.dadata.ru/suggestions/api/4_1/rs'
+
 const DADATA_HEADERS = {
   'Content-Type': 'application/json',
   'Accept': 'application/json'
 }
 
+const SUGGESTION_METHODS = {
+  'fio': 'POST',
+  'address': 'POST',
+  'party': 'POST',
+  'email': 'POST',
+  'bank': 'POST'
+}
+
+const DADATA_PARTY_STATUSES = [
+  'ACTIVE',
+  'LIQUIDATING',
+  'LIQUIDATED'
+]
+
+const DADATA_PARTY_TYPES = [
+  'LEGAL',
+  'INDIVIDUAL'
+]
+
 export default class DaDataModel extends BaseModel {
   constructor (root, token) {
     super(root)
+    this.addHeaders(DADATA_HEADERS)
     this.setToken(token)
 
     this
@@ -19,8 +41,29 @@ export default class DaDataModel extends BaseModel {
 
     // FIO CONTAINER
     .addContainer('fio extends base', {
-      'parts': 'array',
+      'parts': 'allow:[null].array',
       'gender': 'string'
+    })
+
+    // ADDRESS CONTAINER
+    .addContainer('address extends base', {
+      'locations': 'allow:[null].array',
+      'locations_boost': 'allow:[null].array',
+      'from_bound': 'allow:[null].bound',
+      'to_bound': 'allow:[null].bound'
+    })
+
+    // PARTY CONTAINER
+    .addContainer('party extends base', {
+      'status': 'allow:[null].array.party_status',
+      'type': 'allow:[null].array.party_types',
+      'locations': 'allow:[null].array',
+    })
+
+    .addFieldProcessorsBulk({
+      bound: val => val && val.value ? val : null,
+      party_status: val => DADATA_PARTY_STATUSES.includes(val) ? val : null,
+      party_types: val => DADATA_PARTY_TYPES.includes(val) ? val : null,
     })
 
     .addModifiersBulk({
@@ -51,4 +94,22 @@ export default class DaDataModel extends BaseModel {
       'Authorization': `Token ${token}`
     })
   }
+
+  // QUERY METHODS
+  suggest (type, options) {
+    if (!this.getContainer(type)) {
+      console.error(`DaDataModel::suggest() Suggestion type "${type}" not found`)
+      return new Promise((resolve, reject) => {
+        reject(`Suggestion type "${type}" not found`)
+      })
+    }
+
+    this[`$${type}`].data = options
+    return this.generateQuery({
+      uri: `${DADATA_API_URL}/suggest/${type}`,
+      method: SUGGESTION_METHODS[type],
+      container: type
+    })()
+  }
+
 }
